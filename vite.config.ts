e2640a4d438db32aa4ec78efde130eb10ff33dcf
Andrii Yahniukov <andrii.yahniukov@icloud.com>
2026-02-23 14:02:55 +0200

    refactor: code review

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
